b787cb326f6c65546afa84fc7f3de6ac745d6a72
FROfficial <89871575+FROfficial@users.noreply.github.com>
2024-03-27 17:30:58 -0400

    Create Eventr.java
    Initial Comments and First Commit

---
+++ Eventr.java
@@ -0,0 +1,26 @@
+/* Code Authors: 
+		Fabian Ramirez - framir2@clemson.edu
+    Colin Wunch - 
+    Tien Nguyen - 
+    Nick Bunge - 
+    Jing Siong Yu -
+*/
+
+
+/*
+	Purpose:
+  	Eventr is program that will simulate a mobile application and our prototype.
+  
+  Functions:
+  	
+*/
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+
+public class Eventr {
+
+}
+
+
