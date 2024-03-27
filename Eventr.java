89898a8d983bd4360ead34d707e7b1403bcdf1ca
FROfficial <89871575+FROfficial@users.noreply.github.com>
2024-03-27 17:42:43 -0400

    Update Eventr.java
    Created the application frame

--- Eventr.java
+++ Eventr.java
@@ -19,8 +19,27 @@ import javax.swing.*;
 import java.awt.*;
 import java.io.IOException;
 
-public class Eventr {
+public class Main extends JFrame {
+    private JPanel image;
 
+    public Main(){
+        initUI();
+    }
+
+    private void initUI(){
+        setTitle("Eventr");
+        setSize(400,800);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        setLocationRelativeTo(null);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() ->{
+           Main program = new Main();
+           program.setVisible(true);
+        });
+    }
 }
 
 
