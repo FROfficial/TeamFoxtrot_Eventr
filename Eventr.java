a33cf9f2cb23e2904eb125645390ad09cee49c2b
FROfficial <89871575+FROfficial@users.noreply.github.com>
2024-03-27 20:42:00 -0400

    Update Eventr.java

--- Eventr.java
+++ Eventr.java
@@ -1,8 +1,8 @@
-/* Code Authors: 
+/* Code Authors:
 		Fabian Ramirez - framir2@clemson.edu
-    Colin Wunch - 
-    Tien Nguyen - 
-    Nick Bunge - 
+    Colin Wunch -
+    Tien Nguyen -
+    Nick Bunge -
     Jing Siong Yu -
 */
 
@@ -10,18 +10,24 @@
 /*
 	Purpose:
   	Eventr is program that will simulate a mobile application and our prototype.
-  
+
   Functions:
-  	
+
 */
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 
 public class Main extends JFrame {
+    // create an array which we will use to randomize events.
+    private int counter = 0;
+    private String[] imageNames = {"eventr_birdwatching_info.png","eventr_birdwatching.png","eventr_volleyball.png","eventr_volleyball_info.png"};
+		private JLabel displayField;
     public Main() {
         initUI();
     }
@@ -32,9 +38,8 @@ public class Main extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         try {
-            ImageIcon image;
-            image = new ImageIcon(getClass().getResource("image.jpg"));
-            JLabel displayField = new Jlabel(image);
+            ImageIcon image = new ImageIcon(getClass().getResource(imageNames[counter]));
+            displayField = new JLabel(image);
             add(displayField);
         } catch (Exception e) {
             e.printStackTrace();
@@ -49,7 +54,13 @@ public class Main extends JFrame {
     private class ButtonClickListener extends MouseAdapter {
         @Override
         public void mouseClicked(MouseEvent e) {
-            // Handle mouse click event here
+            counter = (counter + 1);
+            try {
+                ImageIcon image = new ImageIcon(getClass().getResource(imageNames[counter]));
+                displayField.setIcon(image);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
         }
     }
 
